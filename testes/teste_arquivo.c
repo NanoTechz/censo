533b725f7474cc2ec52373132e24a6c1c8ac4303
Nanson Augusto <nanson.augusto@hotmail.com>
2013-11-14 11:06:12 -0200

    Tentando ler os aquivos gravados.

---
+++ testes/teste_arquivo.c
@@ -0,0 +1,128 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "../arquivo.h"
+
+#define FILENAME "teste.txt"
+#define TAM 50
+#define QTD 50
+#define null NULL
+
+typedef struct _teste
+{
+	char nome[50];
+	struct _teste *next;
+} teste;
+
+teste *first = NULL;
+teste *last = NULL;
+
+teste * criarNo(int i){
+	teste *no;
+
+	no = (teste *)(malloc(sizeof(teste)));
+	strcpy(no->nome, "teste 00");
+	no->nome[6]= 48 + i/10;
+	no->nome[7]= 48 + i%10;
+	no->next = null;
+
+	return no;
+}
+
+void gerarLista(){
+	int i;
+	teste *no;
+
+	for (i = 0; i < QTD; ++i)
+	{
+		if(i == 0){
+			first = criarNo(i+1);
+			last = first;
+		}else{
+			no = criarNo(i+1);
+			last->next = no;
+			last = no;
+		}
+	}
+}
+
+void imprimirNo(teste *no){
+	printf("Nome: %s\n\tEndereco prox: %p\n", no->nome, no->next);
+}
+
+void imprimirLista(){
+	teste *no;
+
+	no = first;
+
+	do{
+		imprimirNo(no);
+		no = no->next;
+	}while(no != null);
+}
+
+void gravar(FILE *arquivo){
+	int i;
+	teste *no;
+
+	if(first == null){
+		return;
+	}
+
+	no = first;
+
+	if(arquivo){
+		do
+			{
+				fseek(arquivo, 0, SEEK_END);
+				fwrite(&no, sizeof(teste), 1, arquivo);
+				no=no->next;
+			} while (no != null);	
+	}
+}
+
+void ler(FILE *arquivo){
+	teste *no;
+
+	if(arquivo){
+		fseek(arquivo, 0, SEEK_SET);
+		
+		while(!feof(arquivo)){
+			fread(&no, sizeof(teste), 1, arquivo);
+			imprimirNo(no);
+		}
+	}
+}
+
+int main(){
+	FILE *arquivo;
+	int opcao;
+
+	arquivo = inicializarArquivo(FILENAME);
+
+
+	do{
+		printf("Escolha:\n");
+		printf("1 - Gravar Lista No arquivo\n");
+		printf("2 - Ler arquivo\n");		
+		scanf("%d", &opcao);
+		
+		switch(opcao){
+			case 1 :
+				gerarLista();
+				gravar(arquivo);
+				break;
+			case 2 : 
+				ler(arquivo);
+				break;
+			default :
+				printf("Opcao invalida.\n");
+				exit(0);
+				break;
+		}	
+	}while(1);
+
+	fclose(arquivo);
+	return 0;
+}
